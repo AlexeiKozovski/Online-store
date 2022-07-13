d35c3dec5ebe4d4f31f5058b1afe4b8978097ee4
AlexeiKozovski <akoz413@mail.ru>
2022-07-13 12:30:51 +0300

    feat: add class Item

---
+++ online-store/src/components/model/item.ts
@@ -0,0 +1,33 @@
+export class Item {
+  num: number;
+  name: string;
+  count: number;
+  year: number;
+  shape: string;
+  color: string;
+  size: string;
+  favorite: boolean;
+
+  constructor(
+    num: number,
+    name: string,
+    count: number,
+    year: number,
+    shape: string,
+    color: string,
+    size: string,
+    favorite: boolean,
+  ) {
+    this.num = num;
+    this.name = name;
+    this.count = count;
+    this.year = year;
+    this.shape = shape;
+    this.color = color;
+    this.size = size;
+    this.favorite = favorite;
+  }
+
+}
+
+export default Item;
