baeb145cf9b2c4d4b97da956cc83da4055367bd9
AlexeiKozovski <akoz413@mail.ru>
2022-08-02 15:26:47 +0300

    fix: fix Item constructor

--- online-store/src/components/model/item.ts
+++ online-store/src/components/model/item.ts
@@ -1,40 +1,12 @@
 export class Item {
-  id: number;
-
-  name: string;
-
-  prise: number;
-
-  year: number;
-
-  manufacturer: string;
-
-  color: string;
-
-  cpu: string;
-
-  favorite: boolean;
-
   constructor(
-    id: number,
-    name: string,
-    prise: number,
-    year: number,
-    manufacturer: string,
-    color: string,
-    cpu: string,
-    favorite: boolean,
-  ) {
-    this.id = id;
-    this.name = name;
-    this.prise = prise;
-    this.year = year;
-    this.manufacturer = manufacturer;
-    this.color = color;
-    this.cpu = cpu;
-    this.favorite = favorite;
-  }
-
+    public id: number,
+    public name: string,
+    public prise: number,
+    public year: number,
+    public manufacturer: string,
+    public color: string,
+    public cpu: string,
+    public favorite: boolean,
+  ) {}
 }
-
-export default Item;
